9b5d7542d1c61fd06ed9788f5203e1850e72d67a
Robera16 <roberaworknehrobwork@gmail.com>
2024-09-25 18:44:47 +0300

    feature: 4-sponsoring-doctype

--- ssv_reutlingen/ssv_reutlingen/doctype/sponsoring/sponsoring.py
+++ ssv_reutlingen/ssv_reutlingen/doctype/sponsoring/sponsoring.py
@@ -3,9 +3,26 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe import _
 
 class Sponsoring(Document):
-	pass
+	def validate(self):
+		self.validate_contribution_type()
+
+	def validate_contribution_type(self):
+		total_contribution_amount = 0
+		for ct in self.contribution_type:
+			total_contribution_amount += ct.amount
+		
+		if total_contribution_amount != self.net_total:
+			frappe.throw(_("The sum of Contribution Type amounts must equal with Net Total"))
+
+	@frappe.whitelist()
+	def calculate_net_total(self):
+		total_net_amount = 0
+		for item in self.sponsoring_items:
+			total_net_amount += item.net_amount
+		return total_net_amount
 
 @frappe.whitelist()
 def calculate_grand_total(net_total):
